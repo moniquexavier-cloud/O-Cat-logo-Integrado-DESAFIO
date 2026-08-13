59fbe93cc8355a34f6b3cdcd48e81f90e4b935a4
Nick <monique.xavier@ba.estudante.senai.br>
2026-08-13 20:30:01 -0300

    Refactor Catalogo.js to fix user ID handling

--- Catalogo.js
+++ Catalogo.js
@@ -14,12 +14,13 @@ const cadastros = [
   { id: 1, nome: "Franca Roland", email: "franca@email.com", senha: '0998765', estado: "Santa Catarina"},
   { id: 2, nome: "Lumian Lee", email: "lumians@email.com", senha: '4378755', estado: "Minas Gerais"},
   { id: 3, nome: "Andrezito Rossi", email: "andrezito@email.com", senha: '3964541', estado: "Santa Catarina"},
-  { id: 3, nome: "Caíque Cerqueira", email: "caique@email.com", senha: '9374563', estado: "Ceará"}
+  { id: 4, nome: "Caíque Cerqueira", email: "caique@email.com", senha: '9374563', estado: "Ceará"}
 ];
 
 // Rota GET para a raiz (/)
 app.get('/', (req, res) => {
   res.json({ mensagem: 'Bem-vindo à nossa API de Cadastro!' });
+  //resposta em formato JSON
 });
 
 // Rota GET para listar os cadastros (/cadastros)
@@ -27,23 +28,34 @@ app.get('/cadastros', (req, res) => {
   res.json(cadastros);
 });
 
-// Rota GET para listar os cadastros filtrados por categoria(/cadastros/filtrar)
+
+// Rota GET para listar os cadastros filtrados por estado (/cadastros/filtrar)
 app.get('/cadastros/filtrar', (req, res) => {
     const { estado } = req.query;
+    //uma requisição query serve para guardar os parametros de uma requisição
 
     if (estado) {
-      const filtrados = cadastros.filter(uf => uf.estado.toLowerCase() === estado.toLowerCase())
+      const filtrados = cadastros.filter(
+        //ele verifica se o array tem estado preenchido e retorna os valores posteriormente
+        uf => uf.estado && uf.estado.toLowerCase() === estado.toLowerCase()
+        //&& é o AND da parte lógica
+        // toLowerCase transforma todas as letras maiúsculas de um texto em letras minúsculas
+      );
       return res.json(filtrados);
+      // O 'return' encerra a rota AQUI se entrar no IF
     }
 
     res.json(cadastros);
+    // Retorna a lista inteira se o usuário colocar somente /cadastros/filtrar sem colocar parametro
 });
 
 // Rota GET para listar os cadastros filtrados na base do id (/cadastros/:id)
 app.get('/cadastros/:id', (req, res) => {
   const {id} = req.params;
-  res.send(`Buscando o usuário com o ID: ${id}`);
+  const usuario = cadastros.find(u => u.id === Number(id));
+  //o Number serve para transformar a atring que era id em um número
 
+  //condição se if é verdadeiro ele mostra o resultado e se if for falso ele dá uma mensagem de erro
    if (usuario) {
     res.json(usuario);
   } else {
@@ -55,7 +67,9 @@ app.get('/cadastros/:id', (req, res) => {
 // Rota POST para adicionar um novo cadastro com ID automático
 app.post('/cadastros', (req, res) => {
   const { nome, email, senha, estado } = req.body;
+  //requisição para POST 
   
+  // se tiver valores já cadastrados ele soma o id + 1 se não ele só fica 1
   const novoCadastro = {
     id: cadastros.length > 0 ? cadastros[cadastros.length - 1].id + 1 : 1,
     nome,
@@ -64,6 +78,7 @@ app.post('/cadastros', (req, res) => {
     estado
   };
 
+  //colocando o cadastro na lista com uma mensagem
   cadastros.push(novoCadastro);
   res.status(201).json({ mensagem: "Cadastro realizado com sucesso!", dadosRecebidos: novoCadastro });
 });
@@ -72,7 +87,10 @@ app.post('/cadastros', (req, res) => {
 app.delete('/cadastros/:id', (req, res) => {
   const { id } = req.params;
   const index = cadastros.findIndex(u => u.id === Number(id));
-  
+  // o find percorre o array
+  // o index é o indice de posição do item que tem aquele ID.
+
+  // se o idice não for -1 somente ele é deletado do array
   if (index !== -1) {
     cadastros.splice(index, 1);
     return res.status(200).json({ mensagem: "Usuário deletado com sucesso!" });
